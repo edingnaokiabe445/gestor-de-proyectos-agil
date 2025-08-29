24f6e3bc3ee1209c6686d08ce5ae88732c7e38dd
edingnaokiabe445 <gigelekici@hotmail.com>
2025-08-28 21:58:45 -0400

    Update comments to improve clarity in Spanish

--- server.js
+++ server.js
@@ -4,17 +4,17 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware to parse JSON and URL-encoded data
+// Middleware para analizar datos JSON y URL codificados
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public')); // Serve static files from 'public' directory
+app.use(express.static('public')); // Servir archivos estáticos desde el directorio 'public'
 
-// Route to serve the index.html file
+// Ruta para servir el archivo index.html
 app.get('/', (req, res) => {
-    res.sendFile(`${__dirname}/public/index.html`); // Send index.html file
+    res.sendFile(`${__dirname}/public/index.html`); // Enviar el archivo index.html
 });
 
-// Starting the server and listening on the specified port
+// Iniciando el servidor y escuchando en el puerto especificado
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`); // Log server status
+    console.log(`Servidor corriendo en http://localhost:${PORT}`); // Registrar el estado del servidor
 });
