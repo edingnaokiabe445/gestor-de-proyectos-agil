875d17f7118a9da2f5a7ea4b7152d0b948d3d032
edingnaokiabe445 <gigelekici@hotmail.com>
2025-08-28 21:50:53 -0400

    Add comments to server.js for better documentation

--- server.js
+++ server.js
@@ -4,14 +4,17 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Middleware to parse JSON and URL-encoded data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+// Route to serve the index.html file
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
+// Starting the server and listening on the specified port
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
